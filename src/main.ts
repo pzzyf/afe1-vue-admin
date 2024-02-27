6cf2dbdd369efd35f841c3e6e73d574c1199e37b
pzzyf <2279948211@qq.com>
2024-02-27 17:15:17 +0800

    pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { setupStore } from "@/store";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+setupStore(app);
+
+app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "node:path";
+
+const root = process.cwd();
+const pathResolve = (pathname: string) => resolve(root, ".", pathname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: [
+      {
+        find: /@\//,
+        replacement: pathResolve("src") + "/",
+      },
+    ],
+  },
+});
