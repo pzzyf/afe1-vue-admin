3da5b16dfa84b36544e548a13288ffd75708b620
pzzyf <2279948211@qq.com>
2024-03-17 22:46:48 +0800

    routes type

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,5 @@
+import type { RouteRecordRaw } from "vue-router";
+
 import type { App } from "vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 
@@ -5,7 +7,7 @@ import { BasicRoute } from "./routes";
 
 export const router = createRouter({
   history: createWebHashHistory(import.meta.env.VITE_PUBLIC_PATH),
-  routes: BasicRoute, // `routes: routes` 的缩写
+  routes: BasicRoute as unknown as RouteRecordRaw[],
 });
 
 export const setupRouter = (app: App) => {
